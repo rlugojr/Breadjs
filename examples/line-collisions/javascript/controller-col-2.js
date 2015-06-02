486c28177f1ae8c22ff4c97791f577245b6ec372
Juan Camilo <juancgr_4@hotmail.com>
2015-06-02 10:46:42 -0500

    Ejemplo colision linea

--- examples/line-collisions/javascript/controller-col-2.js
+++ examples/line-collisions/javascript/controller-col-2.js
@@ -1,6 +1,6 @@
 
 
-var xob = 100,yob = 12,num_circ = 7,circles=[];
+var xob = 100,yob = 12,num_circ = 390,circles=[];
 	
 	lin = Bread.createThing({
 		x:0,
@@ -42,13 +42,13 @@ var xob = 100,yob = 12,num_circ = 7,circles=[];
 	env.addIt(lin);
 	for (var n = 0,x = 5,y = 80; n < num_circ; n++) {
 
-			x = Bread.random(0,80)
-			y = Bread.random(0,130)
+			x = Bread.random(0,710)
+			y = Bread.random(0,7)
 			circles.push(Bread.createThing({
 				x:x,
 				y:y,
 				//'solid-rectangle':[45 , 132],
-				'solid-circle':3,
+				'solid-circle':2.5,
 				'fill-color' : '#777'
 			}))
 			env.addIt(circles[n])
@@ -56,21 +56,24 @@ var xob = 100,yob = 12,num_circ = 7,circles=[];
 	var colisions = []
 	var angle = -40;
 	directions = [];
-	var xl = 335 , yl = 280;
+	var xl = 335 , yl = 280, x2l = 600 , y2l = 310;
 	//circ1.setCloseness( 0.9 )
 	env.animation( function() {
 
 		lin.unSetGravity();
 		//rec1.setRotationDeg(angle - 30)
-		xl -= 0.05;
-		yl -= 0.05;
+		xl -= 0.075;
+		yl -= 0.075;
+		x2l += 0.1;
+		y2l += 0.1;
 		lin.setLinePoint([xl,yl],2)
+		lin.setLinePoint([x2l,y2l],4)
 
 		for (var n = 0; n < num_circ; n++) {
 			colisions = lin.pointCollision([circles[n].x , circles[n].y])
 
 			if(colisions.length > 0)
-				circles[n].bounce( 0.22, -0.7);
+				circles[n].bounce( 0.09, -2.1);
 
 		};
 		
